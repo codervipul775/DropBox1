54811316c21dbc693b85c656bf12b6ca5cd4e12b
codervipul775 <ydvvipul2005@gmail.com>
2025-04-13 01:35:14 +0530

    Ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Home from './pages/Home'
 import Upload from './pages/Upload'
 import Download from './pages/Download'
+import Layout from './components/Layout'
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'system',
+    useSystemColorMode: true,
+  },
+  colors: {
+    purple: {
+      50: '#f5e9ff',
+      100: '#dbc1ff',
+      200: '#c199ff',
+      300: '#a770ff',
+      400: '#8d48ff',
+      500: '#741fff',
+      600: '#5a15cc',
+      700: '#400e99',
+      800: '#270866',
+      900: '#100233',
+    },
+  },
+})
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/download" element={<Download />} />
-      </Routes>
-    </Router>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/download" element={<Download />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ChakraProvider>
   )
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,69 @@
+import { Box, Container, Flex, Button, Heading, useColorModeValue, useColorMode } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
+import { FaMoon, FaSun } from 'react-icons/fa'
+
+const Layout = ({ children }) => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const bgColor = useColorModeValue('white', 'gray.800')
+  const borderColor = useColorModeValue('gray.200', 'gray.700')
+
+  return (
+    <Box minH="100vh" bg={useColorModeValue('gray.50', 'gray.900')}>
+      <Box 
+        as="nav" 
+        position="fixed" 
+        w="100%" 
+        zIndex={10} 
+        bg={bgColor} 
+        borderBottom="1px solid" 
+        borderColor={borderColor}
+        px={4}
+        py={2}
+      >
+        <Container maxW="container.xl">
+          <Flex justify="space-between" align="center">
+            <Heading 
+              as={RouterLink} 
+              to="/" 
+              size="lg" 
+              color="purple.500"
+              _hover={{ textDecoration: 'none' }}
+            >
+              DropBox
+            </Heading>
+            <Flex gap={4} align="center">
+              <Button
+                as={RouterLink}
+                to="/upload"
+                variant="ghost"
+                colorScheme="purple"
+              >
+                Upload
+              </Button>
+              <Button
+                as={RouterLink}
+                to="/download"
+                variant="ghost"
+                colorScheme="purple"
+              >
+                Download
+              </Button>
+              <Button
+                onClick={toggleColorMode}
+                variant="ghost"
+                color="current"
+              >
+                {colorMode === 'light' ? <FaMoon /> : <FaSun />}
+              </Button>
+            </Flex>
+          </Flex>
+        </Container>
+      </Box>
+      <Box pt="72px">
+        {children}
+      </Box>
+    </Box>
+  )
+}
+
+export default Layout
